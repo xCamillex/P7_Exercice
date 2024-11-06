637df579588b93bda6e0485562fcb9a3bc9b28cc
Camille Brasselet <xcamille2605x@gmail.com>
2024-11-06 21:56:31 +0100

    Etape 3.7 : Ajouts d'un dataset pour les données de sommeil dans un callback "onCreate"

--- app/src/main/java/com/openclassrooms/arista/data/database/AppDatabase.kt
+++ app/src/main/java/com/openclassrooms/arista/data/database/AppDatabase.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.openclassrooms.arista.data.dao.ExerciseDtoDao
 import com.openclassrooms.arista.data.dao.SleepDtoDao
 import com.openclassrooms.arista.data.dao.UserDtoDao
 import com.openclassrooms.arista.data.entity.ExerciseDto
 import com.openclassrooms.arista.data.entity.SleepDto
 import com.openclassrooms.arista.data.entity.UserDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 @Database(entities = [UserDto::class, SleepDto::class, ExerciseDto::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
@@ -18,21 +23,51 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDtoDao(): ExerciseDtoDao
 
 
+    private class AppDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.sleepDtoDao(), database.userDtoDao())
+                }
+            }
+        }
+    }
+
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-
-        fun getDatabase(context: Context): AppDatabase {
+        fun getDatabase(context: Context, coroutineScope: CoroutineScope): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "app_database"
-                ).build()
+                    "AristaDB"
+                )
+                    .addCallback(AppDatabaseCallback(coroutineScope))
+                    .build()
                 INSTANCE = instance
                 instance
             }
+        }
+
+        suspend fun populateDatabase(sleepDao: SleepDtoDao, userDtoDao: UserDtoDao) {
+
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(1).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 480, quality = 4
+                )
+            )
+            sleepDao.insertSleep(
+                SleepDto(
+                    startTime = LocalDateTime.now().minusDays(2).atZone(ZoneOffset.UTC).toInstant()
+                        .toEpochMilli(), duration = 450, quality = 3
+                )
+            )
         }
     }
 }
