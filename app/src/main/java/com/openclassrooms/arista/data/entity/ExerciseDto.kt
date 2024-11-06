310537954ce2a37f3f739f0f961e3bfe7cabc165
Camille Brasselet <xcamille2605x@gmail.com>
2024-11-06 19:07:03 +0100

    Etape 3.2 : Ajout du package entity et DTO ExerciseDto

---
+++ app/src/main/java/com/openclassrooms/arista/data/entity/ExerciseDto.kt
@@ -0,0 +1,28 @@
+package com.openclassrooms.arista.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "exercise")
+data class ExerciseDto(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long = 0,
+
+
+    @ColumnInfo(name = "start_time")
+    var startTime: Long,
+
+
+    @ColumnInfo(name = "duration")
+    var duration: Int,
+
+
+    @ColumnInfo(name = "category")
+    var category: String,
+
+
+    @ColumnInfo(name = "intensity")
+    var intensity: Int
+)
